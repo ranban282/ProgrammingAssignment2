9d07a2906cea096478cee0916a9ff0d506f53362
ranban282 <ranban282@gmail.com>
2015-09-16 19:56:17 +0530

    R Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## This is a helper function which creates a matrix 'object', and allows 
+## the user to get and set its internal matrix, as well as get and set the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+	cachedMatrixInverse <- NULL
+	set <- function(y)
+	{
+		x <<- y
+		cachedMatrixInverse <<- NULL
+	}		
+	get <- function () x
+	setInvertedMatrix <- function(invertedMatrix) cachedMatrixInverse <<- invertedMatrix
+	getInvertedMatrix <- function() cachedMatrixInverse
+	list(set = set, get=get,setInvertedMatrix=setInvertedMatrix,getInvertedMatrix=getInvertedMatrix)
 }
 
 
-## Write a short comment describing this function
-
+## This function calls the above helper function to check 
+## if the inverse of the matrix exists. If so, that matrix is returned.
+## Otherwise, the inverse of the matrix is computed, x's inverted matrix is set, and the inverted matrix is returned.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	invertedMatrix <- x$getInvertedMatrix()
+	if(!is.null(invertedMatrix))
+	{
+		message("Getting cached data.")
+		return(invertedMatrix)
+
+	}
+	matrixToInvert=x$get()
+	invertedMatrix <- solve(matrixToInvert,...)
+	x$setInvertedMatrix(invertedMatrix)
+	invertedMatrix
 }
